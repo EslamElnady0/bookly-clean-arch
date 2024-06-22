a58ed365329e1084e83d49eeadb2657caa58d775
eslam <eslamelnady254@gmail.com>
2024-06-22 09:01:42 +0300

    create fetch featured books use case

---
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
