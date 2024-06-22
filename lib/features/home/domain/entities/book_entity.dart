b81933e8d822925a769d37222b4d5fb4920088c3
eslam <eslamelnady254@gmail.com>
2024-06-22 08:03:42 +0300

    created book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,14 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String author;
+  final String price;
+  final String rating;
+
+  BookEntity(
+      {required this.image,
+      required this.title,
+      required this.author,
+      required this.price,
+      required this.rating});
+}
